63aa7f5d99218dab320969a57f0c6d19a3a8163a
Ivann <youenn.tillard@outlook.com>
2020-08-16 03:26:03 +0200

    controls

--- src/ft_display.c
+++ src/ft_display.c
@@ -6,7 +6,7 @@
 /*   By: yotillar <yotillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 14:44:36 by yotillar          #+#    #+#             */
-/*   Updated: 2020/08/16 01:20:12 by yotillar         ###   ########.fr       */
+/*   Updated: 2020/08/16 03:25:40 by yotillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,6 @@ int	ft_exit(t_win *win)
 	return(0);
 }
 
-int	keyboard(int key, t_win *win)
-{
-	printf("Key pressed!\n");
-/*	if (win == NULL)
-		printf("Win dont exist\n");*/
-	if(key == ESC)
-		ft_exit(win);
-	return (0);
-}
-
-//change param pour prendre *game)
 void	ft_start_display(t_game game, char **map)
 {
 
@@ -51,8 +40,8 @@ void	ft_start_display(t_game game, char **map)
 
 	printf(RESET);
 	printf("Hooking...\n");
-	mlx_hook(game.win.winp, 2, KEY_PRESS_M, &keyboard, &game.win);
-	mlx_hook(game.win.winp, 3, KEY_RELEASE_M, &keyboard, &game.win);
+	mlx_hook(game.win.winp, 2, KEY_PRESS_M, &keyboard, &game);
+	mlx_hook(game.win.winp, 3, KEY_RELEASE_M, &keyboard, &game);
 	//mlx_hook(xserv.winp, 4, BUTTON_PRESS_M, &mouse, &xserv);
 	//mlx_hook(xserv.winp, 5, BUTTON_RELEASE_M, &mouse, &xserv);
 	printf("\nXserv start looping, waiting for events:\n\n");
